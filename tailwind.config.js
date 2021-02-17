bf970ecd06f29e8700fa628d19d2036e394f0378
marianna <marianna@primeflow.com>
2021-02-17 09:58:45 +0100

    integrated typescript and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    './client/**/*.html',
+    './client/**/*.jsx',
+    './client/**/*.tsx',
+    './imports/ui/**/*.js',
+    './imports/ui/**/*.jsx',
+    './imports/ui/**/*.tsx',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
